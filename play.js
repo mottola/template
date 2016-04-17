501e6337b0abef03a52dfa1b6f2390c5dbcca87e
mottola <rickmottola@Ricks-MacBook.local>
2016-04-17 11:54:42 -0600

    template push

---
+++ play.js
@@ -0,0 +1,13 @@
+//var total = 0;
+//var count = 1;
+//while (count <= 10) {
+//    total += count;
+//    count + 1;
+//}
+//console.log(total);
+
+function logConsole {
+    console.log(sum(range(1, 10)));
+}
+
+logConsole();
